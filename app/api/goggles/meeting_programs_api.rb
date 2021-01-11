226d1719e23d009823b82443024f8e531051ae60
steveoro <steve.alloro@gmail.com>
2021-01-11 19:09:30 +0100

    added /meeting_programs GET (+ list)

---
+++ app/api/goggles/meeting_programs_api.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+module Goggles
+  # = Goggles API v3: MeetingProgram API Grape controller
+  #
+  #   - version:  7.060
+  #   - author:   Steve A.
+  #   - build:    20210111
+  #
+  class MeetingProgramsAPI < Grape::API
+    helpers APIHelpers
+
+    format        :json
+    content_type  :json, 'application/json'
+
+    resource :meeting_program do
+      # GET /api/:version/meeting_program/:id
+      #
+      # == Returns:
+      # The MeetingProgram instance matching the specified +id+ as JSON.
+      # See GogglesDb::MeetingProgram#to_json for structure details.
+      #
+      desc 'MeetingProgram details'
+      params do
+        requires :id, type: Integer, desc: 'MeetingProgram ID'
+      end
+      route_param :id do
+        get do
+          check_jwt_session
+
+          GogglesDb::MeetingProgram.find_by_id(params['id'])
+        end
+      end
+    end
+
+    resource :meeting_programs do
+      # GET /api/:version/meeting_programs
+      #
+      # Given a *required* meeting_id plus other optional filtering parameters,
+      # this will return the paginated list of MeetingPrograms found for the specified values.
+      #
+      # == Returns:
+      # The list of MeetingPrograms for the specified filtering parameters as an array of JSON objects.
+      # Returns the exact matches for all parameter values.
+      #
+      # *Pagination* links are stored and returned in the response headers.
+      # - 'Link': list of request links for last & next data pages, separated by ", "
+      # - 'Total': total data rows found
+      # - 'Per-Page': total rows per page
+      # - 'Page': current page
+      #
+      # See official API blueprint docs for more info.
+      # See GogglesDb::MeetingProgram#to_json for structure details.
+      #
+      desc 'List MeetingPrograms'
+      params do
+        requires :meeting_id, type: Integer, desc: 'associated Meeting ID'
+        optional :meeting_session_id, type: Integer, desc: 'optional: associated MeetingSession ID'
+        optional :meeting_event_id, type: Integer, desc: 'optional: associated MeetingEvent ID'
+        optional :category_type_id, type: Integer, desc: 'optional: associated CategoryType ID'
+        optional :gender_type_id, type: Integer, desc: 'optional: associated GenderType ID'
+        use :pagination
+      end
+      paginate
+      get do
+        check_jwt_session
+
+        filtering_conditions = { 'meetings.id': params['meeting_id'] }
+        filtering_conditions.merge!('meeting_sessions.id': params['meeting_session_id']) if params['meeting_session_id'].present?
+        filtering_conditions.merge!(filtering_hash_for(params, %w[meeting_event_id category_type_id gender_type_id]) || {})
+
+        paginate(
+          GogglesDb::MeetingProgram.joins(:meeting, :meeting_session)
+                                 .includes(:meeting, :meeting_session)
+                                 .where(
+                                   ActiveRecord::Base.sanitize_sql_for_conditions(filtering_conditions)
+                                 )
+        )
+      end
+    end
+  end
+end
--- spec/api/goggles/meeting_programs_api_spec.rb
+++ spec/api/goggles/meeting_programs_api_spec.rb
@@ -4,25 +4,25 @@ require 'rails_helper'
 require 'support/api_session_helpers'
 require 'support/shared_api_response_behaviors'
 
-RSpec.describe Goggles::MeetingEventsAPI, type: :request do
+RSpec.describe Goggles::MeetingProgramsAPI, type: :request do
   include GrapeRouteHelpers::NamedRouteMatcher
   include ApiSessionHelpers
 
   let(:api_user) { FactoryBot.create(:user) }
   let(:jwt_token) { jwt_for_api_session(api_user) }
-  let(:fixture_row) { FactoryBot.create(:meeting_event) }
+  let(:fixture_row) { FactoryBot.create(:meeting_program) }
   let(:fixture_headers) { { 'Authorization' => "Bearer #{jwt_token}" } }
 
   # Enforce domain context creation
   before(:each) do
-    expect(fixture_row).to be_a(GogglesDb::MeetingEvent).and be_valid
+    expect(fixture_row).to be_a(GogglesDb::MeetingProgram).and be_valid
     expect(api_user).to be_a(GogglesDb::User).and be_valid
     expect(jwt_token).to be_a(String).and be_present
   end
 
-  describe 'GET /api/v3/meeting_event/:id' do
+  describe 'GET /api/v3/meeting_program/:id' do
     context 'when using valid parameters,' do
-      before(:each) { get(api_v3_meeting_event_path(id: fixture_row.id), headers: fixture_headers) }
+      before(:each) { get(api_v3_meeting_program_path(id: fixture_row.id), headers: fixture_headers) }
       it 'is successful' do
         expect(response).to be_successful
       end
@@ -32,41 +32,40 @@ RSpec.describe Goggles::MeetingEventsAPI, type: :request do
     end
 
     context 'when using an invalid JWT,' do
-      before(:each) { get api_v3_meeting_event_path(id: fixture_row.id), headers: { 'Authorization' => 'you wish!' } }
+      before(:each) { get api_v3_meeting_program_path(id: fixture_row.id), headers: { 'Authorization' => 'you wish!' } }
       it_behaves_like 'a failed auth attempt due to invalid JWT'
     end
 
     context 'when requesting a non-existing ID,' do
-      before(:each) { get(api_v3_meeting_event_path(id: -1), headers: fixture_headers) }
+      before(:each) { get(api_v3_meeting_program_path(id: -1), headers: fixture_headers) }
       it_behaves_like 'an empty but successful JSON response'
     end
   end
   #-- -------------------------------------------------------------------------
   #++
 
-  describe 'GET /api/v3/meeting_events/' do
-    let(:fixture_meeting_event) do
-      GogglesDb::MeetingEvent.joins(:meeting, :meeting_session)
-                             .includes(:meeting, :meeting_session)
-                             .select('meetings.id, meeting_sessions.id')
-                             .distinct.limit(500)
-                             .sample
+  describe 'GET /api/v3/meeting_programs/' do
+    let(:fixture_meeting_program) do
+      GogglesDb::MeetingProgram.joins(:meeting).includes(:meeting)
+                               .select('meetings.id')
+                               .distinct.limit(500)
+                               .sample
     end
-    let(:fixture_meeting) { fixture_meeting_event.meeting }
-    let(:fixture_meeting_session) { fixture_meeting_event.meeting_session }
+    let(:fixture_meeting) { fixture_meeting_program.meeting }
+    let(:fixture_meeting_event) { fixture_meeting_program.meeting_event }
     let(:default_per_page) { 25 }
     # Make sure the Domain contains the expected seeds:
     before(:each) do
-      expect(fixture_meeting_event).to be_a(GogglesDb::MeetingEvent).and be_valid
+      expect(fixture_meeting_program).to be_a(GogglesDb::MeetingProgram).and be_valid
       expect(fixture_meeting).to be_a(GogglesDb::Meeting).and be_valid
-      expect(fixture_meeting_session).to be_a(GogglesDb::MeetingSession).and be_valid
+      expect(fixture_meeting_event).to be_a(GogglesDb::MeetingEvent).and be_valid
     end
 
     context 'when using a valid authentication' do
       context 'without any additional filters (only meeting_id),' do
-        let(:expected_row_count) { fixture_meeting.meeting_events.count }
+        let(:expected_row_count) { fixture_meeting.meeting_programs.count }
 
-        before(:each) { get(api_v3_meeting_events_path, params: { meeting_id: fixture_meeting.id }, headers: fixture_headers) }
+        before(:each) { get(api_v3_meeting_programs_path, params: { meeting_id: fixture_meeting.id }, headers: fixture_headers) }
         it 'is successful' do
           expect(expected_row_count).to be_positive
           expect(response).to be_successful
@@ -75,16 +74,16 @@ RSpec.describe Goggles::MeetingEventsAPI, type: :request do
         it_behaves_like 'successful multiple row response either with OR without pagination links'
       end
 
-      context 'when filtering by a specific meeting_session_id,' do
-        let(:expected_row_count) { fixture_meeting_session.meeting_events.count }
+      context 'when filtering by a specific meeting_event_id,' do
+        let(:expected_row_count) { fixture_meeting_event.meeting_programs.count }
 
         before(:each) do
           expect(expected_row_count).to be_positive
           get(
-            api_v3_meeting_events_path,
+            api_v3_meeting_programs_path,
             params: {
               meeting_id: fixture_meeting.id,
-              meeting_session_id: fixture_meeting_session.id
+              meeting_event_id: fixture_meeting_event.id
             },
             headers: fixture_headers
           )
@@ -95,12 +94,12 @@ RSpec.describe Goggles::MeetingEventsAPI, type: :request do
     end
 
     context 'when using an invalid JWT,' do
-      before(:each) { get(api_v3_meeting_events_path, params: { meeting_id: fixture_meeting.id }, headers: { 'Authorization' => 'you wish!' }) }
+      before(:each) { get(api_v3_meeting_programs_path, params: { meeting_id: fixture_meeting.id }, headers: { 'Authorization' => 'you wish!' }) }
       it_behaves_like 'a failed auth attempt due to invalid JWT'
     end
 
     context 'when filtering by a non-existing value,' do
-      before(:each) { get(api_v3_meeting_events_path, params: { meeting_id: -1 }, headers: fixture_headers) }
+      before(:each) { get(api_v3_meeting_programs_path, params: { meeting_id: -1 }, headers: fixture_headers) }
       it_behaves_like 'an empty but successful JSON list response'
     end
   end
