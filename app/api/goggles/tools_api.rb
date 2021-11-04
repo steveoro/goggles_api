ce694de8ff1353f208f1cabc94a848a535dfe5d1
steveoro <steve.alloro@gmail.com>
2021-11-04 18:22:11 +0100

    added /tools/compute_score (& time) w/ specs & blueprints

--- app/api/goggles/tools_api.rb
+++ app/api/goggles/tools_api.rb
@@ -3,9 +3,9 @@
 module Goggles
   # = Goggles API v3: Tools API: EntryTime finder
   #
-  #   - version:  7.071
+  #   - version:  7-0.3.37
   #   - author:   Steve A.
-  #   - build:    20210126
+  #   - build:    20211104
   #
   class ToolsAPI < Grape::API
     helpers APIHelpers
@@ -18,15 +18,15 @@ module Goggles
       #
       # Returns the suggested entry time given the specified parameters (see below).
       # For more details on the actual strategy used, see:
-      # - GogglesDb::TimingFinders::Factory
-      # - GogglesDb::CmdFindEntryTime
+      # - <tt>GogglesDb::TimingFinders::Factory</tt>
+      # - <tt>GogglesDb::CmdFindEntryTime</tt>
       #
       # == Params:
-      # - swimmer_id
-      # - meeting_id (optional)
-      # - event_type_id
-      # - pool_type_id
-      # - entry_time_type_id (optional)
+      # - <tt>swimmer_id</tt>
+      # - <tt>meeting_id</tt> (optional)
+      # - <tt>event_type_id</tt>
+      # - <tt>pool_type_id</tt>
+      # - <tt>entry_time_type_id</tt> (optional)
       #
       # == Returns:
       #
@@ -72,6 +72,123 @@ module Goggles
 
         { 'label': command.result.to_s, 'timing': command.result } if command.success?
       end
+      #-- ---------------------------------------------------------------------
+      #++
+
+      # GET /api/:version/tools/compute_score
+      #
+      # Computes either the resulting score given a specific timing or the target
+      # timing itself given the resulting score, depending on which parameter is supplied.
+      #
+      # For more details on the actual strategy used, see:
+      # - <tt>GogglesDb::Calculators::Factory</tt>
+      # - <tt>GogglesDb::CmdSelectScoreCalculator</tt>
+      # - <tt>GogglesDb::Calculators::BaseStrategy</tt>
+      #
+      # == Params:
+      # - <tt>pool_type_id</tt>
+      # - <tt>event_type_id</tt>
+      #
+      # Plus, at least:
+      # - <tt>badge_id</tt> (optional)
+      # - <tt>hundredths</tt> (optional, is +score+ is specified)
+      # - <tt>seconds</tt> (optional, is +score+ is specified)
+      # - <tt>minutes</tt> (optional, is +score +is specified)
+      # - <tt>score</tt> (optional, if all the timing parameters are supplied)
+      #
+      # Or, alternatively, all these:
+      # - <tt>season_id</tt> (optional, if the <tt>badge_id</tt> is supplied)
+      # - <tt>gender_type_id</tt> (optional, if the <tt>badge_id</tt> is supplied)
+      # - <tt>category_type_id</tt> (optional, if the <tt>badge_id</tt> is supplied)
+      #
+      # Optional override:
+      # - <tt>standard_points</tt> (optional, defaults to 1000)
+      #
+      # == Returns:
+      #
+      # A zero score on error, a positive floating value otherwise.
+      # The resulting score or target timing will be stored inside the following
+      # JSON structure:
+      #
+      #   {
+      #     # Result or target score (depending on the parameters supplied):
+      #     "score": 845.65,
+      #
+      #     # Result or target timing (as above):
+      #     "timing": {
+      #       "hundredths": 47, "seconds": 5, "minutes": 2, "hours": 0, "days": 0
+      #     },
+      #     "timing_label": "2'05\"47",
+      #
+      #     "standard_points": 1000.0,
+      #     "display_label": "MS 4x50 SL [25 M] - 200-239, FIN MASTER 2019/2020, MIX",
+      #     "short_label": "MS 4x50 SL [25 M] - 200-239 MIX",
+      #     "standard_timing_label": "1'48\"79",
+      #     "standard_timing": {
+      #       "id": 16509,
+      #       "minutes": 1, "seconds": 48, "hundredths": 79,
+      #       "season_id": 192, "gender_type_id": 3,
+      #       "pool_type_id": 1, "event_type_id": 32,
+      #       "category_type_id": 1309,
+      #       # [...]
+      #      }
+      #   }
+      #
+      desc 'Compute result score or target timing, depending on the parameters supplied'
+      params do
+        requires :pool_type_id, type: Integer, desc: 'PoolType ID for the event'
+        requires :event_type_id, type: Integer, desc: 'EventType ID for the event'
+
+        optional :minutes, type: Integer, desc: 'optional: target or result time minutes (required when the target score has to be computed)'
+        optional :seconds, type: Integer, desc: 'optional: target or result time seconds (required when the target score has to be computed)'
+        optional :hundredths, type: Integer, desc: 'optional: target or result time hundredths of seconds (required to compute the target score)'
+        optional :score, type: Integer, desc: 'optional: target score (required when the target timing is desired)'
+        optional :standard_points, type: Integer, desc: 'optional: standard base points override; default: 1000'
+
+        optional :badge_id, type: Integer, desc: 'optional: Badge ID (as alternative instead of Season + Gender + Category)'
+        optional :season_id, type: Integer, desc: 'optional: Season ID for the event (as alternative instead of Badge)'
+        optional :gender_type_id, type: Integer, desc: 'optional: GenderType ID for the event (as alternative instead of Badge)'
+        optional :category_type_id, type: Integer, desc: 'optional: CategoryType ID for the event (as alternative instead of Badge)'
+      end
+      get :compute_score do
+        check_jwt_session
+
+        # Retrieve values:
+        pool_type  = reject_unless_found(params['pool_type_id'], GogglesDb::PoolType)
+        event_type = reject_unless_found(params['event_type_id'], GogglesDb::EventType)
+        timing = Timing.new(minutes: params['minutes'], seconds: params['seconds'], hundredths: params['hundredths'])
+        target_score = params['score']
+        standard_points = params['standard_points'] || 1000.0
+
+        badge = GogglesDb::Badge.find_by(id: params['badge_id'])
+        season = GogglesDb::Season.find_by(id: params['season_id'])
+        gender_type = GogglesDb::GenderType.find_by(id: params['gender_type_id'])
+        category_type = GogglesDb::CategoryType.find_by(id: params['category_type_id'])
+
+        command = GogglesDb::CmdSelectScoreCalculator.call(
+          pool_type: pool_type, event_type: event_type, badge: badge,
+          season: season, gender_type: gender_type, category_type: category_type
+        )
+
+        return { 'score': 0, 'timing': timing.to_json, errors: command.errors } unless command.success?
+
+        score_value = command.result.compute_for(timing, standard_points: standard_points) if timing.present?
+        timing = command.result.timing_from(target_score.to_i) if target_score.present?
+        standard_timing = command.result.standard_timing
+
+        {
+          'score': score_value || target_score,
+          'timing_label': timing.to_s,
+          'timing': timing,
+          'standard_points': standard_points,
+          'display_label': standard_timing&.decorate&.display_label,
+          'short_label': standard_timing&.decorate&.short_label,
+          'standard_timing_label': standard_timing&.to_timing.to_s,
+          'standard_timing': standard_timing
+        }
+      end
+      #-- ---------------------------------------------------------------------
+      #++
     end
   end
 end
--- spec/api/goggles/tools_api_spec.rb
+++ spec/api/goggles/tools_api_spec.rb
@@ -32,12 +32,6 @@ RSpec.describe Goggles::ToolsAPI, type: :request do
   end
   let(:fixture_swimmer) { fixture_mir.swimmer }
   let(:fixture_meeting) { fixture_mir.meeting }
-  let(:api_request_params) do
-    {
-      swimmer_id: fixture_swimmer.id, meeting_id: fixture_meeting.id,
-      event_type_id: fixture_event_type.id, pool_type_id: fixture_pool_type.id
-    }
-  end
 
   before do
     expect(api_user).to be_a(GogglesDb::User).and be_valid
@@ -48,8 +42,47 @@ RSpec.describe Goggles::ToolsAPI, type: :request do
     expect(fixture_meeting).to be_a(GogglesDb::Meeting).and be_valid
     expect(fixture_swimmer).to be_a(GogglesDb::Swimmer).and be_valid
   end
+  #-- -------------------------------------------------------------------------
+  #++
 
-  describe 'GET /api/v3/Tools/:entity_name' do
+  # REQUIRES/ASSUMES:
+  # - 'result_hash': the parsed JSON response from the API call.
+  shared_examples_for('result_hash including a Timing object with a dedicated label field') do |label_key, timing_field_key|
+    it "returns a '#{label_key}' field text, with format <nn'nn\"nn> when non-empty" do
+      expect(result_hash).to have_key(label_key)
+      expect(result_hash[label_key]).to match(/\d{1,2}'\d{1,2}"\d{1,2}/) if result_hash[label_key].present?
+    end
+
+    it "returns a '#{timing_field_key}' hash, when the StandardTime is found (otherwise nil)" do
+      expect(result_hash).to have_key(timing_field_key)
+      if result_hash[timing_field_key].present?
+        expect(result_hash[timing_field_key]).to be_a(Hash)
+        expect(result_hash[timing_field_key]).to have_key('minutes')
+        expect(result_hash[timing_field_key]).to have_key('seconds')
+        expect(result_hash[timing_field_key]).to have_key('hundredths')
+      end
+    end
+
+    it "has the '#{label_key}' field equal to the default string representation of its '#{timing_field_key}' field (when non-empty)" do
+      timing = Timing.new(
+        hundredths: result_hash[timing_field_key]&.fetch('hundredths', nil),
+        seconds: result_hash[timing_field_key]&.fetch('seconds', nil),
+        minutes: result_hash[timing_field_key]&.fetch('minutes', nil),
+        hours: result_hash[timing_field_key]&.fetch('hours', nil),
+        days: result_hash[timing_field_key]&.fetch('days', nil)
+      )
+      expect(result_hash[label_key]).to eq(timing.to_s) if result_hash[timing_field_key].present?
+    end
+  end
+
+  describe 'GET /api/v3/tools/find_entry_time' do
+    let(:api_request_params) do
+      {
+        swimmer_id: fixture_swimmer.id, meeting_id: fixture_meeting.id,
+        event_type_id: fixture_event_type.id, pool_type_id: fixture_pool_type.id
+      }
+    end
+
     context 'when using a valid authentication' do
       context 'with valid parameters (corresponding to existing entities)' do
         before { get(api_v3_tools_find_entry_time_path, params: api_request_params, headers: fixture_headers) }
@@ -62,30 +95,10 @@ RSpec.describe Goggles::ToolsAPI, type: :request do
           expect(result_hash).to be_a(Hash)
         end
 
-        it "returns a non-empty 'label' field text, with format <nn'nn\"nn>" do
-          expect(result_hash).to have_key('label')
-          expect(result_hash['label']).to be_present
-          expect(result_hash['label']).to match(/\d{1,2}'\d{1,2}"\d{1,2}/)
-        end
-
-        it "returns a non-empty 'timing' hash" do
-          expect(result_hash).to have_key('timing')
-          expect(result_hash['timing']).to be_a(Hash)
-          expect(result_hash['timing']).to have_key('minutes')
-          expect(result_hash['timing']).to have_key('seconds')
-          expect(result_hash['timing']).to have_key('hundredths') # (sic: "hundredths")
-        end
-
-        it "has the 'label' field equal to the default string representation of its 'timing' field" do
-          timing = Timing.new(
-            hundredths: result_hash['timing']['hundredths'],
-            seconds: result_hash['timing']['seconds'],
-            minutes: result_hash['timing']['minutes'],
-            hours: result_hash['timing']['hours'],
-            days: result_hash['timing']['days']
-          )
-          expect(result_hash['label']).to eq(timing.to_s)
-        end
+        it_behaves_like(
+          'result_hash including a Timing object with a dedicated label field',
+          'label', 'timing'
+        )
       end
 
       context 'when using valid parameters but during Maintenance mode,' do
@@ -129,4 +142,180 @@ RSpec.describe Goggles::ToolsAPI, type: :request do
       it_behaves_like('a failed auth attempt due to invalid JWT')
     end
   end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  describe 'GET /api/v3/tools/compute_score' do
+    let(:api_score_request_params) do
+      {
+        event_type_id: fixture_event_type.id, pool_type_id: fixture_pool_type.id,
+        minutes: fixture_mir.minutes, seconds: fixture_mir.seconds, hundredths: fixture_mir.hundredths,
+        badge_id: fixture_mir.badge_id
+      }
+    end
+    let(:api_timing_request_params) do
+      {
+        event_type_id: fixture_event_type.id, pool_type_id: fixture_pool_type.id,
+        score: fixture_mir.standard_points.to_i, badge_id: fixture_mir.badge_id
+      }
+    end
+
+    context 'when using a valid authentication' do
+      context 'with valid parameters for computing the score,' do
+        before { get(api_v3_tools_compute_score_path, params: api_score_request_params, headers: fixture_headers) }
+
+        let(:result_hash) { JSON.parse(response.body) }
+
+        it_behaves_like('a successful request that has positive usage stats')
+
+        it 'returns a valid JSON hash' do
+          expect(result_hash).to be_a(Hash)
+        end
+
+        it "returns a non-empty 'score' field (the computed score)" do
+          expect(result_hash).to have_key('score')
+          expect(result_hash['score']).to be_present
+          # In case the StandardTiming wasn't found we only know for sure it will return the default standard points:
+          expect(result_hash['score']).to eq(1000) if result_hash['standard_timing'].blank?
+          # NOTE
+          # We cannot assert the exact value because we are using fixture results that may
+          # refer to standard timings that may have been updated after the result score was computed.
+        end
+
+        it "returns a non-empty 'standard_points' field" do
+          expect(result_hash).to have_key('standard_points')
+          expect(result_hash['standard_points']).to be_present
+          expect(result_hash['standard_points']).to eq(1000)
+        end
+
+        it "returns the standard timing 'display_label' field (when the standard time is found)" do
+          expect(result_hash).to have_key('display_label')
+          expect(result_hash['display_label']).to be_present if result_hash['standard_timing'].present?
+        end
+
+        it "returns the standard timing 'short_label' field (when the standard time is found)" do
+          expect(result_hash).to have_key('short_label')
+          expect(result_hash['short_label']).to be_present if result_hash['standard_timing'].present?
+        end
+
+        it_behaves_like(
+          'result_hash including a Timing object with a dedicated label field',
+          'timing_label', 'timing'
+        )
+
+        it_behaves_like(
+          'result_hash including a Timing object with a dedicated label field',
+          'standard_timing_label', 'standard_timing'
+        )
+      end
+
+      context 'with valid parameters for computing the target time,' do
+        before { get(api_v3_tools_compute_score_path, params: api_timing_request_params, headers: fixture_headers) }
+
+        let(:result_hash) { JSON.parse(response.body) }
+
+        it_behaves_like('a successful request that has positive usage stats')
+
+        it 'returns a valid JSON hash' do
+          expect(result_hash).to be_a(Hash)
+        end
+
+        it "returns a non-empty 'score' field, equal to the specified score parameter" do
+          expect(result_hash).to have_key('score')
+          expect(result_hash['score']).to be_present
+          expect(result_hash['score']).to eq(fixture_mir.standard_points.to_i)
+        end
+
+        it "has the 'timing' Hash field as the computed target timing" do
+          expect(result_hash).to have_key('timing')
+          expect(result_hash['timing']).to be_a(Hash)
+          timing = Timing.new(
+            hundredths: result_hash['timing']['hundredths'],
+            seconds: result_hash['timing']['seconds'],
+            minutes: result_hash['timing']['minutes'],
+            hours: result_hash['timing']['hours'],
+            days: result_hash['timing']['days']
+          )
+          # In case the StandardTiming wasn't found we only know for sure it will return zero:
+          expect(timing).to eq(Timing.new) if result_hash['standard_timing'].blank?
+          # NOTE
+          # We cannot assert the exact value because we are using fixture results that may
+          # refer to standard timings that may have been updated after the result score was computed.
+        end
+
+        it "returns a non-empty 'standard_points' field" do
+          expect(result_hash).to have_key('standard_points')
+          expect(result_hash['standard_points']).to be_present
+          expect(result_hash['standard_points']).to eq(1000)
+        end
+
+        it "returns the standard timing 'display_label' field (when the standard time is found)" do
+          expect(result_hash).to have_key('display_label')
+          expect(result_hash['display_label']).to be_present if result_hash['standard_timing'].present?
+        end
+
+        it "returns the standard timing 'short_label' field (when the standard time is found)" do
+          expect(result_hash).to have_key('short_label')
+          expect(result_hash['short_label']).to be_present if result_hash['standard_timing'].present?
+        end
+
+        it_behaves_like(
+          'result_hash including a Timing object with a dedicated label field',
+          'timing_label', 'timing'
+        )
+
+        it_behaves_like(
+          'result_hash including a Timing object with a dedicated label field',
+          'standard_timing_label', 'standard_timing'
+        )
+      end
+
+      context 'when using valid parameters for computing the score but during Maintenance mode,' do
+        before do
+          GogglesDb::AppParameter.maintenance = true
+          get(api_v3_tools_compute_score_path, params: api_score_request_params, headers: fixture_headers)
+          GogglesDb::AppParameter.maintenance = false
+        end
+
+        it_behaves_like('a request refused during Maintenance (except for admins)')
+      end
+
+      context 'when using valid parameters for computing the target time but during Maintenance mode,' do
+        before do
+          GogglesDb::AppParameter.maintenance = true
+          get(api_v3_tools_compute_score_path, params: api_timing_request_params, headers: fixture_headers)
+          GogglesDb::AppParameter.maintenance = false
+        end
+
+        it_behaves_like('a request refused during Maintenance (except for admins)')
+      end
+
+      context 'with invalid or missing parameters' do
+        let(:failing_request_params) do
+          { event_type_id: fixture_event_type.id, pool_type_id: fixture_pool_type.id }
+        end
+
+        before { get(api_v3_tools_compute_score_path, params: failing_request_params, headers: fixture_headers) }
+
+        it 'is successful anyway (because it is a kind of warning)' do
+          expect(response).to be_successful
+        end
+
+        it 'responds with an errors sub-hash with the actual error message in it' do
+          result = JSON.parse(response.body)
+          expect(result).to have_key('errors')
+          expect(result['errors']).to have_key('msg')
+          expect(result['errors']['msg']).to eq('Invalid or missing constructor parameters')
+        end
+      end
+    end
+
+    context 'when using an invalid JWT,' do
+      before { get(api_v3_tools_compute_score_path, params: api_score_request_params, headers: { 'Authorization' => 'you wish!' }) }
+
+      it_behaves_like('a failed auth attempt due to invalid JWT')
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
 end
