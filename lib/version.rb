4dddda820094d6cf78e23a5a8666e90847f17e42
steveoro <steve.alloro@gmail.com>
2024-04-15 14:16:53 +0200

    re-sync w/ base engine & bundle update; also split some CircleCI jobs trying to improve build timings; rel. 0.7.09

--- lib/version.rb
+++ lib/version.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 #
-# == Versioning codes
+# = Version module
+# - author: Steve A.
 #
-#   - version:  7-0.7.08
-#   - author:   Steve A.
-#
-# Framework version number usually differs from core engine versioning number.
+# Framework version may differ from core engine versioning number.
 # Actual Semantic versioning code is stored into 'goggles_db'.
-# To be sure getting the correct Semantic versioning number, either use:
+# To be sure getting the correct Semantic versioning number use:
 #
 # > require 'goggles_db/version'
 # > GogglesDb::VERSION
@@ -29,10 +27,10 @@ module Version
   MINOR = '7'
 
   # Patch version.
-  PATCH = '08'
+  PATCH = '09'
 
   # Current build version.
-  BUILD = '20240409'
+  BUILD = '20240415'
 
   # Full versioning for the current release.
   FULL = "#{MAJOR}.#{MINOR}.#{PATCH} (#{CORE}-#{BUILD})".freeze
@@ -41,5 +39,5 @@ module Version
   SEMANTIC = "#{MAJOR}.#{MINOR}.#{PATCH}".freeze
 
   # Current internal DB version (independent from migrations and framework release)
-  DB = '2.07.1'
+  DB = '2.07.2'
 end
