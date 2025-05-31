d233d5e7ab452c98d525b6b59f9ebb37e13480b6
steveoro <steve.alloro@gmail.com>
2025-05-31 20:04:33 +0200

    updated version number

--- lib/version.rb
+++ lib/version.rb
@@ -27,10 +27,10 @@ module Version
   MINOR = '8'
 
   # Patch version.
-  PATCH = '06'
+  PATCH = '07'
 
   # Current build version.
-  BUILD = '20250102'
+  BUILD = '20250531'
 
   # Full versioning for the current release.
   FULL = "#{MAJOR}.#{MINOR}.#{PATCH} (#{CORE}-#{BUILD})".freeze
@@ -39,5 +39,5 @@ module Version
   SEMANTIC = "#{MAJOR}.#{MINOR}.#{PATCH}".freeze
 
   # Current internal DB version (independent from migrations and framework release)
-  DB = '2.07.6'
+  DB = '2.08.07'
 end
